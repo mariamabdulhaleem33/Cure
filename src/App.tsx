50b6235c3acfd50756e916e7674ab668b1e7eec8
AhmedElabrass25 <ahmedelabrass2035@gmail.com>
2025-12-25 13:12:32 +0200

    add logic of booking page and doctor information and reviews

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AppRoutes from "./routes/index";
-
+import { Toaster } from "@/components/ui/sonner";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +12,7 @@ function App() {
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
+      <Toaster />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
---
+++ src/components/create_booking/hooks/useReview.ts
@@ -0,0 +1,23 @@
+// hooks/useDoctor.ts
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import type { IReview } from "../types";
+
+export const useDoctorReviews = (doctorId?: number | null, token?: string) => {
+  return useQuery<IReview[]>({
+    queryKey: ["doctorReviews", doctorId],
+    queryFn: async () => {
+      const res = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/api/reviews/doctor/${doctorId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+          },
+        }
+      );
+      return res.data.data;
+    },
+    enabled: !!doctorId,
+  });
+};
